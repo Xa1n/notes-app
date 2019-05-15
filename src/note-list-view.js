2e9c5be46ba87fb37366738a52e2f31cb6fbf444
Xa1n <xhs893@gmail.com>
2019-05-15 10:41:11 +0100

    Passes getHTML test by concatenating strings to the newly created array within the method

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,14 +1,16 @@
 (function(exports) {
     function NoteListView(noteList) {
-
+        this.noteList = noteList
     };
 
     NoteListView.prototype.getHTML = function() {
-
+        
+        var openUl = "<ul><li><div>"
+        var closeUl = "</div></li></ul>"
         var newList = noteList.list.map(i => i.returnText())
 
-        return newList.join("</div></li><li><div>")
-    }
+        return openUl + newList.join("</div></li><li><div>") + closeUl
+    };
 
     exports.NoteListView = NoteListView;
 })(this);
